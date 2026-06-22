cf14d383cec234d10ceee8b40fdbbcbf648f33be
RAMOS SERVICES <falecomprramos@gmail.com>
2026-06-22 12:34:15 -0300

    fix: date-fns ptBR import for v3

--- src/app/cliente/calendario/page.tsx
+++ src/app/cliente/calendario/page.tsx
@@ -2,7 +2,7 @@
 
 import { Calendar, dateFnsLocalizer } from "react-big-calendar"
 import { format, parse, startOfWeek, getDay } from "date-fns"
-import ptBR from "date-fns/locale/pt-BR"
+import { ptBR } from "date-fns/locale"
 
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
